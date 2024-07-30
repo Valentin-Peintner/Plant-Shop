5d9e4255d39d7a5681571ee5dd08a8dffe777074
Valentin Peintner <valentin.peintner@gmx.at>
2024-07-30 12:29:28 +0200

    First Commit

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
 
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="index.css">
+  <title>Planty's</title>
+</head>
+<body>
+  <div id="navbar-container">
+    <div class="company-header">
+      <div>
+        <img src="img/logo.avif" alt="logo">
+        <h1>Planty</h1>
+      </div>
+    </div>
+    <div class="navbar">
+      <div class="navbar-list">
+        <ul>
+          <li>Home</li>
+          <li>Profil</li>
+          <li>Kontakt</li>
+        </ul>
+      </div>    
+    </div>
+  </div>
+</body>
+</html>
